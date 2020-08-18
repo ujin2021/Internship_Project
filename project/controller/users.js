c584854ae8121520e9ff43f75caec1ff138846c9
ujin2021 <ujin2021@naver.com>
2020-08-18 12:15:15 +0900

    경로 수정, jwt middleware 적용

--- project/controller/users.js
+++ project/controller/users.js
@@ -1,23 +1,24 @@
-const express = require('express');
-const router = express.Router();
+const express = require('express')
+const router = express.Router()
 
-const jwt = require('jsonwebtoken');
+const jwt = require('jsonwebtoken')
+const generateToken = require('../config/token').generateToken
 
-const key = require('../config/settings.js').secretKey;
-const crypto = require('crypto');
-require('console-stamp')(console, 'yyyy/mm/dd HH:MM:ss.l');
-
+const key = require('../config/settings.js').secretKey
+const crypto = require('crypto')
+require('console-stamp')(console, 'yyyy/mm/dd HH:MM:ss.l')
+/*
 async function createToken(user_no) { // 토큰 생성
     // 기본 알고리즘 : HMAC SHA256
     // 알고리즘 변경 할 수 있다. https://github.com/auth0/node-jsonwebtoken#usage
-    const token = jwt.sign({user_no: user_no}, key.secretKey);
+    const token = jwt.sign({user_no: user_no}, key.secretKey, {expiresIn:'7d'});
     return token;
-}
+}*/
 
 exports.emailCheckAPI = async (req, res) => { // 이메일 중복체크
     try {
         const params = [req.body['email']]
-        const result = await res.pool.query(`SELECT * FROM USERS WHERE  user_email = ?`, params)
+        const result = await res.pool.query(`SELECT * FROM USERS WHERE user_email = ?`, params)
 		if (result[0].length === 0) {
             res.status(200).json({'status' : 200, 'msg' : `가입 가능한 이메일 주소 입니다.`})
         } else {
@@ -79,7 +80,7 @@ exports.loginAPI =  async(req, res) => { // 회원 로그인(토큰 생성)
         db_pw += await decipher.final('utf8');
 
         if (user_password === db_pw){
-            let token = await createToken(result[0][0].user_no);
+            let token = await generateToken(result[0][0].user_no);
             await res.status(200).json({'status' : 200, 'token' : token});
         } else {
             res.status(400).json({'status' : 400, 'msg' : `비밀번호가 일치하지 않습니다.`})
@@ -87,7 +88,7 @@ exports.loginAPI =  async(req, res) => { // 회원 로그인(토큰 생성)
     }catch (e) {
         console.error(e)
         if (e instanceof TypeError){
-            await res.status(200).json({'status' : 204, 'msg' : `존재하지 않는 계정입니다.`});
+            await res.status(400).json({'status' : 400, 'msg' : `존재하지 않는 계정입니다.`});
         }
         await res.status(503).json(e)
     }
--- project/routes/category.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const categoryController = require('../controller/category');
-
-router.get('/', categoryController.listAPI);
-router.post('/', categoryController.selectCategory)
-
-module.exports = router;
