02521f439fa3c76efdc7265560993a4068d7fb42
ujin2021 <ujin2021@naver.com>
2020-08-18 12:13:32 +0900

    jwt middleware 생성

--- project/app.js
+++ project/app.js
@@ -2,12 +2,13 @@ const express = require('express');
 const request = require('request');
 const database = require('./config/database')
 const dbConfig = require('./config/dbConfig').local
+const jwtDecode = require('./config/token').jwtDecode
 const app = express();
 app.use(express.json()) // bodyParser 사용 설정
 
 const indexRouter = require('./routes/index.js')
 const userRouter = require('./routes/users.js') // email_check, signup, login
-const categoryRouter = require('./routes/category.js') // category list, product list
+const listRouter = require('./routes/lists.js') // category list, product list, review list
 const productRouter = require('./routes/products.js')
 
 app.use(database.sql.pool('pool', {
@@ -20,7 +21,8 @@ app.use(database.sql.pool('pool', {
 
 app.use('/', indexRouter)
 app.use('/users', userRouter)
-app.use('/category', categoryRouter)
+app.use('/lists', listRouter)
+
 app.use('/products', productRouter)
 
 app.listen(3000, function(){
--- project/routes/products.js
+++ project/routes/products.js
@@ -2,9 +2,10 @@ const express = require('express')
 const router = express.Router()
 const productController = require('../controller/products')
 
-router.post('/review_list', productController.reviewList) // 해당 product의 review 들을 띄워줌
-router.post('/review', productController.productReview) // 해당 product 에 review 등록
-router.post('/like', productController.productLike) // 해당 product 찜하기
-router.post('/log', productController.productLog) // 해당 product 조회 기록
+const { jwtDecode } = require('../config/token')
+
+router.post('/review', jwtDecode(), productController.productReview) // 해당 product 에 review 등록
+router.post('/like', jwtDecode(), productController.productLike) // 해당 product 찜하기
+router.post('/log', jwtDecode(), productController.productLog) // 해당 product 조회 기록
 
 module.exports = router
